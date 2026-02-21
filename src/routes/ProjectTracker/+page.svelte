6a1d40a6bf9cd5048602a5bf10a1c9519488c33b
vk22006 <kishorev22006@gmail.com>
2026-02-21 22:45:17 +0530

    添加Flowbite组件库

--- src/routes/ProjectTracker/+page.svelte
+++ src/routes/ProjectTracker/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Header from '../Header.svelte';
+    import { Alert } from "flowbite-svelte";
     // // import Sidebar from './sidebar.svelte';
     // import { fly, fade } from "svelte/transition";
     // import { onMount } from "svelte";
@@ -8,6 +9,13 @@
 </script>
 
 <Header {page} />
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
 
 <a href="./ProjectTracker">你的项目</a><br>
 <a href="./ProgressDashboard">进步</a><br>
