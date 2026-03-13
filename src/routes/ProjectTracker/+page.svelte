97d109155f97e5efada9381ee3e09b821ee7faef
vk22006 <kishorev22006@gmail.com>
2026-03-13 23:08:47 +0530

    refactor(sidebar): 更新新的toggle功能 #1

--- src/routes/ProjectTracker/+page.svelte
+++ src/routes/ProjectTracker/+page.svelte
@@ -1,52 +1,51 @@
 <script lang="ts">
-    import Header from '../Header.svelte';
-    import { Sidebar, SidebarGroup, SidebarItem, SidebarButton, uiHelpers } from "flowbite-svelte";
-    import { page } from "$app/stores";
+	import Header from '../Header.svelte';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarButton, uiHelpers } from 'flowbite-svelte';
+	import { page } from '$app/stores';
 
-    let title = $state('你的项目');
+	let title = $state('你的项目');
 
-    let activeUrl = $derived($page.url.pathname);
-    // import PlusPlaceholder from "$utils/PlusPlaceholder.svelte";
-    const spanClass = "flex-1 ms-3 whitespace-nowrap";
-    const demoSidebarUi = uiHelpers();
-    let isDemoOpen = $state(false);
-    const closeDemoSidebar = demoSidebarUi.close;
-    $effect(() => {
-        isDemoOpen = demoSidebarUi.isOpen;
-    });
+	let activeUrl = $derived($page.url.pathname);
+	// import PlusPlaceholder from "$utils/PlusPlaceholder.svelte";
+	const spanClass = 'flex-1 ms-3 whitespace-nowrap';
+	const demoSidebarUi = uiHelpers();
+	demoSidebarUi.isOpen = true;
+	let isDemoOpen = $state(true);
+	const closeDemoSidebar = demoSidebarUi.close;
+	$effect(() => {
+		isDemoOpen = demoSidebarUi.isOpen;
+	});
 </script>
 
+<div class="relative flex min-h-screen content-start">
+	<Sidebar
+		{activeUrl}
+		backdrop={false}
+		isOpen={isDemoOpen}
+		closeSidebar={closeDemoSidebar}
+		params={{ x: -50, duration: 50 }}
+		class="z-50 h-full {isDemoOpen ? '' : 'md:!-translate-x-full'}"
+		position="absolute"
+		classes={{ nonactive: 'p-2', active: 'p-2' }}
+	>
+		<SidebarGroup>
+			<SidebarItem label="TODO清单" href="/"></SidebarItem>
 
-<div class="flex">
-<SidebarButton onclick={demoSidebarUi.toggle} class="mb-2" />
-  <Sidebar
-    {activeUrl}
-    backdrop={false}
-    isOpen={isDemoOpen}
-    closeSidebar={closeDemoSidebar}
-    params={{ x: -50, duration: 50 }}
-    class="z-50 h-full"
-    position="absolute"
-    classes={{ nonactive: "p-2", active: "p-2" }}
-  >
-    <SidebarGroup>
+			<SidebarItem label="你的项目" href="/ProjectTracker"></SidebarItem>
 
-      <SidebarItem label="TODO清单" href="/">
-      </SidebarItem>
+			<SidebarItem label="进步" href="/ProgressDashboard"></SidebarItem>
 
-      <SidebarItem label="你的项目" href="/ProjectTracker">
-      </SidebarItem>
+			<SidebarItem label="设置" href="/Settings"></SidebarItem>
+		</SidebarGroup>
+	</Sidebar>
 
-      <SidebarItem label="进步" href="/ProgressDashboard">
-      </SidebarItem>
-
-      <SidebarItem label="设置" href="/Settings">
-      </SidebarItem>
-
-    </SidebarGroup>
-  </Sidebar>
-
-  <main class="flex-1 p-6">
-    <Header {title}/>
-  </main>
+	<main class="flex-1 p-6 transition-all duration-300 {isDemoOpen ? 'ml-64' : ''}">
+		<div class="mb-4 flex items-center gap-4">
+			<SidebarButton
+				onclick={demoSidebarUi.toggle}
+				class="!inline-flex rounded-lg p-2 text-gray-500 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"
+			/>
+		</div>
+		<Header {title} />
+	</main>
 </div>
